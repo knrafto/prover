6d027f0886281b226eda7312d663ffb9ab26b916
Kyle Raftogianis <knrafto@gmail.com>
2019-09-21 18:05:02 -0700

    Farther into parsing

--- prover/Main.hs
+++ prover/Main.hs
@@ -6,11 +6,34 @@ import System.Environment
 import System.IO
 
 import Data.Text (Text)
+import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
+
+-- AST
+type Module = [Defn]
+
+data Defn = Defn
+    { defnName :: Text
+    , defnParams :: Params
+    , defnType :: Expr
+    , defnBody :: Expr
+    } deriving (Show)
+
+type Params = [(Text, Expr)]
+
+data Expr
+    = Var Text
+    | Call Expr [Expr]
+    | Sigma Params Expr
+    | Pi Params Expr
+    | Lambda Params Expr
+    | Arrow Expr Expr
+    | Eq Expr Expr
+    deriving (Show)
 
 -- Lots of parsing stuff from https://markkarpov.com/megaparsec/megaparsec.html
 type Parser = Parsec Void Text
@@ -18,14 +41,11 @@ type Parser = Parsec Void Text
 lineComment :: Parser ()
 lineComment = L.skipLineComment "#"
 
-scn :: Parser ()
-scn = L.space space1 lineComment empty
-
 sc :: Parser ()
-sc = L.space (void $ some (char ' ' <|> char '\t')) lineComment empty
+sc = L.space space1 lineComment empty
 
 lexeme :: Parser a -> Parser a
-lexeme = L.lexeme (scn <* eof)
+lexeme = L.lexeme sc
 
 isWordChar :: Char -> Bool
 isWordChar c = c `notElem` (" \t\r\n\f\v#()," :: [Char])
@@ -37,14 +57,35 @@ identifier :: Parser Text
 identifier = lexeme . try $ do
     w <- takeWhile1P (Just "word character") isWordChar
     when (w `elem` reservedWords) $
-        fail $ "keyword " ++ show w ++ " is reserved and cannot be used as an identifier"
+        fail $ "keyword " ++ Text.unpack w ++ " is reserved and cannot be used as an identifier"
     return w
   where
     reservedWords :: [Text]
     reservedWords = [":", ":=", "=", "Σ", "Π", "λ", "→"]
 
+symbol :: Char -> Parser ()
+symbol c = lexeme (void $ char c)
+
+expr :: Parser Expr
+expr = Var <$> identifier  -- TODO
+
+params :: Parser Params
+params = between (symbol '(') (symbol ')') $ param `sepBy1` symbol ','
+  where
+    param = (,) <$> identifier <* reservedWord ":" <*> expr
+
+defn :: Parser Defn
+defn = L.nonIndented sc $ do
+    name <- identifier
+    ps <- params
+    reservedWord ":"
+    t <- expr
+    reservedWord ":="
+    b <- expr
+    return $ Defn { defnName = name, defnParams = ps, defnType = t, defnBody = b }
+
 compile :: Text -> IO ()
-compile = parseTest (identifier <* eof)
+compile = parseTest (many defn <* eof)
 
 main :: IO ()
 main = getArgs >>= \args -> case args of
