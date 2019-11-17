8f3c55addce63538bf5b9c4aad7a7a9851f6a17f
Kyle Raftogianis <knrafto@gmail.com>
2019-11-16 22:41:42 -0800

    JSON parsing in vscode

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,26 +1,69 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as child_process from 'child_process';
+
 import * as vscode from 'vscode';
+
+// TODO: configure
+let binaryPath =
+    '/Users/knrafto/code/prover/.stack-work/dist/x86_64-osx/Cabal-2.4.0.1/build/prover/prover';
+
+// Run a command to completion, reporting its output and exit code.
+function runCommand(
+    command: string, args: string[],
+    callback: (stdout: string, stderr: string) => void) {
+  console.log('Running command:', command, args.join(' '));
+  var child = child_process.spawn(command, args);
+
+  var stdout = '';
+  var stderr = '';
+
+  child.stdout.setEncoding('utf8');
+  child.stdout.on('data', (data) => {
+    stdout += data.toString();
+  });
+
+  child.stderr.setEncoding('utf8');
+  child.stderr.on('data', (data) => {
+    stderr += data.toString();
+  });
+
+  child.on('close', () => {
+    callback(stdout, stderr);
+  });
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Extension "prover" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "prover" is now active!');
+  let disposable = vscode.workspace.onDidSaveTextDocument((document) => {
+    let path = document.uri.path;
+    console.log('Document saved:', path);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    if (!path.endsWith('.pf')) {
+      return;
+    }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+    runCommand(binaryPath, ['--json', document.uri.path], (stdout, stderr) => {
+      if (stdout.length !== 0) {
+        console.log('Output:\n' + stdout);
+      }
+      if (stderr.length !== 0) {
+        console.log('Error:\n' + stderr);
+      }
 
-	context.subscriptions.push(disposable);
+      var json;
+      try {
+        json = JSON.parse(stdout);
+      } catch (e) {
+        console.log('Error decoding JSON:', e);
+        return;
+      }
+    });
+  });
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
