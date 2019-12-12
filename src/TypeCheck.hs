c5f6aa7f31b8cc12fdd939260e1ccec0a6f51f06
Kyle Raftogianis <knrafto@gmail.com>
2019-12-11 17:52:49 -0800

    Use ElabM (kinda)

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -52,11 +52,20 @@ exprType :: Expr Tc -> Type
 exprType e = case ann e of
     TcAnn _ (_, ty) -> ty
 
-newtype ElabM a = ElabM { runElabM :: WriterT (DList Constraint) TcM a }
+newtype ElabM a = ElabM { unElabM :: WriterT (DList Constraint) TcM a }
     deriving
         ( Functor, Applicative, Monad, MonadFail, MonadIO
         , MonadError Diagnostic, MonadState TcState, MonadWriter (DList Constraint)
         )
+
+runElabM :: ElabM a -> TcM a
+runElabM m = do
+    (a, _) <- runWriterT (unElabM m)
+    checkSolved
+    return a
+
+checkEqual :: Range -> Ctx -> Type -> Term -> Term -> ElabM ()
+checkEqual l ctx ty t1 t2 = ElabM (lift (unify l ctx ty t1 t2))
 
 typeCheck :: [Statement N] -> TcM [Statement Tc]
 typeCheck = mapM typeCheckStatement
@@ -65,25 +74,25 @@ typeCheckStatement :: Statement N -> TcM (Statement Tc)
 typeCheckStatement = \case
     Syntax.Define n mty body -> do
         let name = unLoc (nameUsage n)
-        body' <- typeCheckExpr C0 [] body
-        mty' <- case mty of
-            Nothing -> return Nothing
-            Just ty -> do
-                ty' <- typeCheckExpr C0 [] ty
-                unify (exprRange body') C0 Universe (exprType body') (exprTerm ty')
-                return (Just ty')
-        checkSolved
+        (body', mty') <- runElabM $ do
+            body' <- typeCheckExpr C0 [] body
+            mty' <- case mty of
+                Nothing -> return Nothing
+                Just ty -> do
+                    ty' <- typeCheckExpr C0 [] ty
+                    checkEqual (exprRange body') C0 Universe (exprType body') (exprTerm ty')
+                    return (Just ty')
+            return (body', mty')
         modify $ \s -> s { tcDefinitions = HashMap.insert name (exprUTerm body') (tcDefinitions s) }
         return (Syntax.Define n mty' body')
     Syntax.Assume n ty -> do
         let name = unLoc (nameUsage n)
-        ty' <- typeCheckExpr C0 [] ty
-        checkSolved
+        ty' <- runElabM $ typeCheckExpr C0 [] ty
         modify $ \s -> s { tcAssumptions = HashMap.insert name (exprTerm ty') (tcAssumptions s) }
         return (Syntax.Assume n ty')
     Syntax.Prove _ _ -> fail "prove not implemented"
 
-typeCheckExpr :: Ctx -> [Text] -> Expr N -> TcM (Expr Tc)
+typeCheckExpr :: Ctx -> [Text] -> Expr N -> ElabM (Expr Tc)
 typeCheckExpr ctx names = \case
     Syntax.Var l n -> do
         let name = unLoc (nameUsage n)
@@ -142,7 +151,7 @@ typeCheckExpr ctx names = \case
         tt <- typeCheckSigma l ctx (exprUTerm a') (weakenUTerm (exprUTerm b'))
         return (Syntax.Times (TcAnn l tt) a' b')
 
-assumption :: Text -> TcM UTerm
+assumption :: Text -> ElabM UTerm
 assumption name = do
     assumptions <- gets tcAssumptions
     case HashMap.lookup name assumptions of
@@ -150,7 +159,7 @@ assumption name = do
         _       -> fail $ "can't find built-in: " ++ Text.unpack name
 
 -- Generate an unknown term and type.
-hole :: Ctx -> TcM UTerm
+hole :: Ctx -> ElabM UTerm
 hole _Γ = do
     -- We generate variables for both the hole itself, and its type. Luckily
     -- for now we don't have to do this forever, since the type of any type is
@@ -162,7 +171,7 @@ hole _Γ = do
 weakenUTerm :: UTerm -> UTerm
 weakenUTerm (t, ty) = (weaken t, weaken ty)
 
-typeCheckParam :: Ctx -> [Text] -> Param N -> Expr N -> TcM (Param Tc, UTerm, Expr Tc)
+typeCheckParam :: Ctx -> [Text] -> Param N -> Expr N -> ElabM (Param Tc, UTerm, Expr Tc)
 typeCheckParam ctx names (n, me) body = do
     let name = unLoc (nameUsage n)
     (me', tt) <- case me of
@@ -175,36 +184,36 @@ typeCheckParam ctx names (n, me) body = do
     body' <- typeCheckExpr (ctx :> (fst tt)) (name : names) body
     return ((n, me'), tt, body')
 
-typeCheckPi :: Range -> Ctx -> UTerm -> UTerm -> TcM UTerm
+typeCheckPi :: Range -> Ctx -> UTerm -> UTerm -> ElabM UTerm
 typeCheckPi l ctx (_A, _Aty) (_B, _Bty) = do
-    unify l ctx Universe _Aty Universe
-    unify l (ctx :> _A) Universe _Bty Universe
+    checkEqual l ctx Universe _Aty Universe
+    checkEqual l (ctx :> _A) Universe _Bty Universe
     return (Pi _A _B, Universe)
 
-typeCheckLambda :: Range -> Ctx -> UTerm -> UTerm -> TcM UTerm
+typeCheckLambda :: Range -> Ctx -> UTerm -> UTerm -> ElabM UTerm
 typeCheckLambda l ctx (_A, _Aty) (b, _B) = do
-    unify l ctx Universe _Aty Universe
+    checkEqual l ctx Universe _Aty Universe
     return (Lam b, Pi _A _B)
 
-typeCheckSigma :: Range -> Ctx -> UTerm -> UTerm -> TcM UTerm
+typeCheckSigma :: Range -> Ctx -> UTerm -> UTerm -> ElabM UTerm
 typeCheckSigma l ctx a b = do
     f  <- assumption "Σ'"
     b' <- typeCheckLambda l ctx a b
     typeCheckApps l ctx f [a, b']
 
-typeCheckApp :: Range -> Ctx -> UTerm -> UTerm -> TcM UTerm
+typeCheckApp :: Range -> Ctx -> UTerm -> UTerm -> ElabM UTerm
 typeCheckApp l ctx (f, fty) (arg, _A) = do
     _B <- freshMeta (ctx :> _A) Universe
-    unify l ctx Universe fty (Pi _A _B)
+    checkEqual l ctx Universe fty (Pi _A _B)
     return (app f [arg], instantiate _B arg)
 
-typeCheckApps :: Range ->  Ctx -> UTerm -> [UTerm] -> TcM UTerm
+typeCheckApps :: Range ->  Ctx -> UTerm -> [UTerm] -> ElabM UTerm
 typeCheckApps _ _   f []           = return f
 typeCheckApps l ctx f (arg : args) = do
     f' <- typeCheckApp l ctx f arg
     typeCheckApps l ctx f' args
 
-typeCheckTuple :: Range -> Ctx -> [UTerm] -> TcM UTerm
+typeCheckTuple :: Range -> Ctx -> [UTerm] -> ElabM UTerm
 typeCheckTuple _ _   []         = error "typeCheckTuple: empty tuple"
 typeCheckTuple _ _   [t       ] = return t
 typeCheckTuple l ctx (a : rest) = do
