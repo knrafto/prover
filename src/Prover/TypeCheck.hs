ceb00eb40aa23c8055401893d20d4d42863db025
Kyle Raftogianis <knrafto@gmail.com>
2020-09-13 15:31:05 -0700

    More solid type-checking foundation

--- src/Prover/TypeCheck.hs
+++ src/Prover/TypeCheck.hs
@@ -41,29 +41,6 @@ createName (C.Name r t) = do
   id <- freshNameId
   return (A.Name id r t)
 
--- | Generate a constraint that two terms (of possibly different types) are
--- equal.
-checkEqualTerms :: Range -> Term -> Type -> Term -> Type -> M ()
-checkEqualTerms _ a tyA b tyB = do
-  ctx <- asks envCtx
-  debugFields "unify terms" $
-    [ "ctx" |: prettyCtx ctx
-    , "a"   |: prettyTerm ctx a
-    , "A"   |: prettyTerm ctx tyA
-    , "b"   |: prettyTerm ctx b
-    , "B"   |: prettyTerm ctx tyB
-    ]
-
--- | Generate a constraint that two types equal.
-checkEqualTypes :: Range -> Type -> Type -> M ()
-checkEqualTypes _ tyA tyB = do
-  ctx <- asks envCtx
-  debugFields "unify types" $
-    [ "ctx" |: prettyCtx ctx
-    , "A"   |: prettyTerm ctx tyA
-    , "B"   |: prettyTerm ctx tyB
-    ]
-
 -- | Type-check with a new binding.
 extendEnv :: Maybe A.Name -> Type -> M a -> M a
 extendEnv n ty = local $ \e -> Env
@@ -80,49 +57,73 @@ lookupLocal t varNames = go 0 varNames
       | A.nameText n == t = Just (i, n)
     go i (_:rest)         = go (i + 1) rest
 
+-- | Generate a constraint that two terms (of possibly different types) are
+-- equal.
+addConstraint :: Range -> Term -> Type -> Term -> Type -> M ()
+addConstraint _ a tyA b tyB = do
+  ctx <- asks envCtx
+  debugFields "created constraint" $
+    [ "ctx" |: prettyCtx ctx
+    , "a"   |: prettyTerm ctx a
+    , "A"   |: prettyTerm ctx tyA
+    , "b"   |: prettyTerm ctx b
+    , "B"   |: prettyTerm ctx tyB
+    ]
+
+-- | Generate expression info for a range, term, and type, while checking that
+-- it matches the expected output type.
+expect :: Range -> Term -> Type -> Type -> M A.ExprInfo
+expect r a tyA tyB = do
+  b <- createMeta tyB
+  addConstraint r a tyA b tyB
+  return (A.ExprInfo r b tyB)
+
 -- | Producing a judgement Γ ⊢ t : A.
-checkExpr :: C.Expr -> M A.Expr
-checkExpr = \case
+checkExpr :: C.Expr -> Type -> M A.Expr
+checkExpr expr outputTy = case expr of
   C.Id      n       -> do
     let r = C.nameRange n
-        t = C.nameText n
+        s = C.nameText n
     e <- ask
-    s <- get
+    state <- get
     case () of
-      _ | Just (v, n) <- lookupLocal t (envVarNames e) -> do
-          let ty   = ctxLookup (envCtx e) v
-              info = A.ExprInfo r (App (Var v) []) ty
-              n'   = A.Name (A.nameId n) r t
-          return $ A.Var info n'
+      _ | Just (v, n) <- lookupLocal s (envVarNames e) -> do
+          let t    = App (Var v) []
+              ty   = ctxLookup (envCtx e) v
+              n'   = A.Name (A.nameId n) r s
+          i <- expect r t ty outputTy
+          return $ A.Var i n'
 
-      _ | Just id <- HashMap.lookup t (globalNames s)
-        , Just ty <- HashMap.lookup id (defTypes s) -> do
-          let info = A.ExprInfo r (App (Def id) []) ty
-              n'   = A.Name id r t
-          return $ A.Def info n'
+      _ | Just id <- HashMap.lookup s (globalNames state)
+        , Just ty <- HashMap.lookup id (defTypes state) -> do
+          let t    = App (Def id) []
+              n'   = A.Name id r s
+          i <- expect r t ty outputTy
+          return $ A.Def i n'
 
-      _ | Just id <- HashMap.lookup t (globalNames s)
-        , Just ty <- HashMap.lookup id (axiomTypes s) -> do
-          let info = A.ExprInfo r (App (Axiom id) []) ty
-              n'   = A.Name id r t
-          return $ A.Axiom info n'
+      _ | Just id <- HashMap.lookup s (globalNames state)
+        , Just ty <- HashMap.lookup id (axiomTypes state) -> do
+          let t    = App (Axiom id) []
+              n'   = A.Name id r s
+          i <- expect r t ty outputTy
+          return $ A.Axiom i n'
 
-      _ -> throwError $ UnboundName r t
+      _ -> throwError $ UnboundName r s
 
   C.Hole    r       -> do
-    ty <- createMeta Type
-    t  <- createMeta ty
-    return $ A.Hole (A.ExprInfo r t ty)
+    t <- createMeta outputTy
+    return $ A.Hole (A.ExprInfo r t outputTy)
 
   C.Type    r       -> do
-    return $ A.Type (A.ExprInfo r Type Type)
+    i <- expect r Type Type outputTy
+    return $ A.Type i
 
   C.Pi      r p e   -> do
     -- Γ ⊢ A : Type
     b@(A.Binding n tyA _) <- checkBinding p
 
     -- Γ, x : A ⊢ B : Type
-    e' <- extendEnv (Just n) tyA $ checkExprIsType e
+    e' <- extendEnv (Just n) tyA $ checkExpr e Type
 
     -- ⟹ Γ ⊢ (Π x : A. B) : Type
     let t   = Pi tyA (A.exprTerm e')
@@ -133,42 +134,44 @@ checkExpr = \case
     b@(A.Binding n tyA _) <- checkBinding p
 
     -- Γ, x : A ⊢ e : B
-    e' <- extendEnv (Just n) tyA $ checkExpr e
+    tyB <- extendEnv (Just n) tyA $ createMeta Type
+    e'  <- extendEnv (Just n) tyA $ checkExpr e tyB
 
     -- ⟹ Γ ⊢ (λ x : A. e) : (Π x : A. B)
     let t   = Lam (A.exprTerm e')
         ty  = Pi tyA (A.exprType e')
-    return $ A.Lam (A.ExprInfo r t ty) b e'
+    i <- expect r t ty outputTy
+    return $ A.Lam i b e'
 
   C.Sigma   r _ _   -> throwError $ Unimplemented r "Σ-types"
 
   C.App     r f a   -> do
     -- Γ ⊢ a : A
-    a' <- checkExpr a
+    tyA <- createMeta Type
+    a'  <- checkExpr a tyA
 
     -- Γ ⊢ f : (Π x : A. B)
-    let tyA = A.exprType a'
     tyB <- extendEnv Nothing tyA (createMeta Type)
-    f'  <- checkExpr f
-    checkEqualTypes r (A.exprType f') (Pi tyA tyB)
+    f'  <- checkExpr f (Pi tyA tyB)
 
     -- ⟹ Γ ⊢ f a : B[a/x]
-    checkApply f'
     -- TODO: a chained application is currently quadratic in the number of
     -- arguments
     let t  = applyTerm (A.exprTerm f') [A.exprTerm a']
         ty = instantiate tyB (A.exprTerm a') 
-    return $ A.App (A.ExprInfo r t ty) f' a'
+    i <- expect r t ty outputTy
+    return $ A.App i f' a'
   C.Arrow   r e1 e2 ->  do
     -- Γ ⊢ A : Type
-    e1' <- checkExprIsType e1
+    e1' <- checkExpr e1 Type
 
     -- Γ ⊢ B : Type
-    e2' <- checkExprIsType e2
+    e2' <- checkExpr e2 Type
 
     -- ⟹ Γ ⊢ (Π _ : A. B) : Type
-    let t   = Pi (A.exprTerm e1') (weaken (A.exprTerm e1'))
-    return $ A.Arrow (A.ExprInfo r t Type) e1' e2'
+    let t = Pi (A.exprTerm e1') (weaken (A.exprTerm e1'))
+    i <- expect r t Type outputTy
+    return $ A.Arrow i e1' e2'
 
   C.Times   r _  _  -> throwError $ Unimplemented r "Σ-types"
 
@@ -179,20 +182,14 @@ checkExpr = \case
     id  <- case HashMap.lookup "Id" (globalNames s) of
       Nothing -> throwError $ MissingBuiltin r "Id"
       Just id -> return id
-    a   <- createMeta Type
-    e1' <- checkExpr e1
-    e2' <- checkExpr e2
-    let t   = App (Axiom id) [a, A.exprTerm e1', A.exprTerm e2']
-    return $ A.Equals (A.ExprInfo r t Type) e1' e2'
+    tyA <- createMeta Type
+    e1' <- checkExpr e1 tyA
+    e2' <- checkExpr e2 tyA
+    let t   = App (Axiom id) [tyA, A.exprTerm e1', A.exprTerm e2']
+    i <- expect r t Type outputTy
+    return $ A.Equals i e1' e2'
 
   C.Pair    r _  _  -> throwError $ Unimplemented r "Σ-types"
-
--- | Produce a judgement Γ ⊢ t : Type.
-checkExprIsType :: C.Expr -> M A.Expr
-checkExprIsType e = do
-  e' <- checkExpr e
-  checkEqualTypes (getRange e') (A.exprType e') Type
-  return e'
 
 -- | Check that an expression can be applied to an argument before constructing
 -- a term for the application (which would otherwise fail).
@@ -211,22 +208,21 @@ checkApply = \case
 checkBinding :: C.Param -> M A.Binding
 checkBinding (n, ann) = do
   n' <- createName n
-  (ty, ann') <- case ann of
+  (ty', ann') <- case ann of
     Nothing -> do
       ty <- createMeta Type
       return (ty, Nothing)
-    Just e -> do
-      e' <- checkExprIsType e
-      return (A.exprTerm e', Just e')
-  return $ A.Binding n' ty ann'
+    Just ty -> do
+      ty' <- checkExpr ty Type
+      return (A.exprTerm ty', Just ty')
+  return $ A.Binding n' ty' ann'
 
 checkDecl :: C.Decl -> M A.Decl
 checkDecl = \case
   C.Define n ann e -> do
     debug $ "checking definition" <+> pretty (C.nameText n) <+> "..."
     b  <- checkBinding (n, ann)
-    e' <- checkExpr e
-    checkEqualTypes (getRange e') (A.bindingType b) (A.exprType e')
+    e' <- checkExpr e (A.bindingType b)
     let n' = A.bindingName b
     modify $ \s -> s
       { globalNames = HashMap.insert (A.nameText n') (A.nameId n') (globalNames s)
@@ -238,7 +234,7 @@ checkDecl = \case
   C.Assume n ty   -> do
     debug $ "checking assumption" <+> pretty (C.nameText n) <+> "..."
     n'  <- createName n
-    ty' <- checkExprIsType ty
+    ty' <- checkExpr ty Type
     modify $ \s -> s
       { globalNames = HashMap.insert (A.nameText n') (A.nameId n') (globalNames s)
       , axiomNames  = HashMap.insert (A.nameId n') n' (axiomNames s)
