d4af49e90579302c90404d61a3b78dc8f61ca700
Kyle Raftogianis <knrafto@gmail.com>
2020-11-20 12:38:21 -0800

    Make search a tiny bit more concrete

--- src/Prover/Search.hs
+++ src/Prover/Search.hs
@@ -4,45 +4,48 @@ import Data.Function
 import Data.List
 import Data.Monoid
 
-import Data.Heap (Heap)
 import Data.Heap qualified as Heap
+
+import Prover.Monad
 
 type Cost = Sum Int
 
 -- | A search node representing the "unsolved goal state".
 data Node = Node
-  { parent :: Maybe Node  -- ^ Parent node (Nothing if start node)
-  , distance :: Cost  -- ^ Distance from start
-  , heuristic :: Cost  -- ^ Heuristic distance to the goal
-    -- TODO: unification problem
+  { nodeParent :: Maybe Node  -- ^ Parent node (Nothing if start node)
+  , nodeDistance :: Cost  -- ^ Distance from start
+  , nodeHeuristic :: Cost  -- ^ Heuristic distance to the goal
+  , nodeProblem :: UnificationProblem
   }
 
-estimatedCost :: Node -> Cost
-estimatedCost node = distance node + heuristic node
+nodeCost :: Node -> Cost
+nodeCost node = nodeDistance node + nodeHeuristic node
 
 instance Eq Node where
-  (==) = (==) `on` estimatedCost
+  (==) = (==) `on` nodeCost
 
 instance Ord Node where
-  compare = compare `on` estimatedCost
+  compare = compare `on` nodeCost
 
 -- | Decides whether a node is a solution.
 isSolved :: Node -> Bool
 isSolved = error "isSolved" -- TODO
 
 -- | Enumerate the children of a node, along 
-children :: Monad m => Node -> m [Node]
+children :: Node -> M [Node]
 children = error "children" -- TODO
 
 -- | Run A* search.
 -- TODO: need history of explored nodes. must be able to reconstruct tree at each step
 -- use node ids?
-search :: Monad m => Cost -> Heap Node -> m ()
-search maxCost heap = case Heap.viewMin heap of
-  Nothing -> error "exhausted search space" -- TODO
-  Just (node, heap)
-    | isSolved node -> error "found a solution" -- TODO
-    | estimatedCost node > maxCost -> error "gave up search" -- TODO
-    | otherwise -> do
-      ns <- children node
-      search maxCost (foldl' (flip Heap.insert) heap ns)
+search :: Cost -> Node -> M ()
+search maxDistance initialNode = go (Heap.singleton initialNode)
+  where
+    go heap = case Heap.viewMin heap of
+      Nothing -> error "exhausted search space" -- TODO
+      Just (node, heap)
+        | isSolved node -> error "found a solution" -- TODO
+        | nodeDistance node > maxDistance -> error "gave up search" -- TODO
+        | otherwise -> do
+          ns <- children node
+          go (foldl' (flip Heap.insert) heap ns)
